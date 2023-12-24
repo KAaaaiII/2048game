d992be352b5cfe85554aad5c61dca21ba3e387c4
KAaaaiII <1870308096@qq.com>
2023-12-24 17:03:58 +0800

    Update source.cpp

--- 2048/2048/source.cpp
+++ 2048/2048/source.cpp
@@ -6,8 +6,20 @@ using namespace std;
 const int N = 4;//边长
 class game2048 {//创建游戏相关的类
 public:
-
-
+	void initializing(string str) {//类的初始化
+		memset(Map, 0, sizeof(Map));//初始化地图二维数组
+		score = 0;
+		maxsq = 0;
+		sumsq = 0;
+		player = str;
+	}
+	void hideCursor() {//隐藏光标操作，这里参考了一些网上的做法，用到头文件windows.h中的函数
+		CONSOLE_CURSOR_INFO cursor;
+		cursor.bVisible = 0;
+		cursor.dwSize = 1;
+		HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleCursorInfo(hOut, &cursor);
+	}
 protected:
 	int Map[N + 2][N + 2];//地图二维数组
 	int score;//分数
